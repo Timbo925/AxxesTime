c1ced978c67c678c4b5209372d2143fc7979d9ec
Rob <rob.kenis@hotmail.com>
2017-09-01 11:38:02 +0200

    move domain package

--- src/main/java/com/axxes/timesheet/time/domain/User.java
+++ src/main/java/com/axxes/timesheet/time/domain/User.java
@@ -1,4 +1,4 @@
-package domain;
+package com.axxes.timesheet.time.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
--- src/main/java/com/axxes/timesheet/time/domain/UserType.java
+++ src/main/java/com/axxes/timesheet/time/domain/UserType.java
@@ -1,4 +1,4 @@
-package domain;
+package com.axxes.timesheet.time.domain;
 
 public enum UserType {
 
