dbbac636759284923d77fff12709eed75017aa6a
Tim Witters <tim.witters@healthconnect.be>
2017-09-01 14:59:12 +0200

    Security Update

--- src/main/java/com/axxes/timesheet/time/config/WebSecurityConfig.java
+++ src/main/java/com/axxes/timesheet/time/config/WebSecurityConfig.java
@@ -1,41 +1,46 @@
 package com.axxes.timesheet.time.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
+@ComponentScan("com.axxes")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static String REALM="MY_TEST_REALM";
+
     @Autowired
-    private RestAuthenticationEntryPoint authenticationEntryPoint;
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("ADMIN");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
-        http.csrf().disable();
-        http
+        http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers( "/hello", "/error").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .antMatchers("//**").hasRole("ADMIN")
+                .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+    @Bean
+    public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint(){
+        return new CustomBasicAuthenticationEntryPoint();
     }
+
+//    /* To allow Pre-flight [OPTIONS] request from browser */
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+//    }
 }
